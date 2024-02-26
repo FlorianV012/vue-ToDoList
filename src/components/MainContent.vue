8a7b46bbf41902cea4c182a439c2887a3fd4f227
fvilmot <fvilmot@gmail.com>
2024-02-26 11:24:53 +0100

    installing vue and TS

---
+++ src/components/MainContent.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>TODO List</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "MainContent",
+  props: {
+    msg: String,
+  },
+});
+</script>
+
+<style></style>
