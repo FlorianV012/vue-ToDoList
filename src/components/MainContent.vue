66aca6a29ff6cfb3a4ec8f98b190ce29cd2e1002
Florian <fvilmot@gmail.com>
2024-02-26 13:56:37 +0100

    displaying tasks and creating the form for adding task

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -1,5 +1,26 @@
 <template>
+  <div class="container">
     <h1>TODO List</h1>
+
+    <form>
+      <div class="form-group">
+        <label for="newTask">Create new task</label>
+        <input
+          class="form-control"
+          type="text"
+          id="newTask"
+          v-model="formData.task"
+        />
+      </div>
+      <button class="btn btn-primary my-3">Create task</button>
+    </form>
+
+    <ul>
+      <li v-for="(task, index) in tasksArray" :key="index">
+        {{ task }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script lang="ts">
@@ -7,10 +28,21 @@ import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "MainContent",
-  props: {
-    msg: String,
+  components: {},
+  data() {
+    return {
+      formData: {
+        task: "",
+      },
+      tasksArray: ["task 1", "task 2", "task 3"],
+    };
   },
+  methods: {},
 });
 </script>
 
-<style></style>
+<style>
+ul {
+  list-style-type: none;
+}
+</style>
